c147d898ea295bf71dd51b9a14362abbf20be8f1
sachinsav <sachin.saw.13@gmai.com>
2021-04-20 20:12:55 +0530

    JS Practice

--- js/first.js
+++ js/first.js
@@ -11,6 +11,15 @@ obj = {
     add:function(a,b){return a+b},
     min: function(a,b){return a-b}
 }
-console.log(add(2,3,4));
+obj2 = Object.create(obj);
+console.log(obj2.__proto__==obj); //true because obj2.__proto__ is obj due to inheritance
+class Person{
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+    }
+}
 console.log(add(2,3,4));
 console.log(obj.desc,obj.add(5,3),obj.min(5,3),typeof obj);
+person_obj = new Person('sachin',21);
+console.log(person_obj,typeof Person,Person.prototype);
